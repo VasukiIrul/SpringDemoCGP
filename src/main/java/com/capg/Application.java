a6402061c728875538953ca8594935794dfca83c
vasuki <vasuki.tce@gmail.com>
2022-04-24 19:07:13 +0530

    serverini

--- src/main/java/com/capg/Application.java
+++ src/main/java/com/capg/Application.java
@@ -3,18 +3,18 @@ package com.capg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	//@Override
+@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringApplication.class);
 	}
