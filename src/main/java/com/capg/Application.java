f57617d3a86dddb189997ce2224c62cb32d0c0d3
vasuki <vasuki.tce@gmail.com>
2022-04-24 19:15:57 +0530

    spring config

--- src/main/java/com/capg/Application.java
+++ src/main/java/com/capg/Application.java
@@ -2,20 +2,15 @@ package com.capg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringApplication.class);
-	}
+
 }
