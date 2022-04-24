65654d621fd742ec6b6aa27c27696cab67323498
vasuki <vasuki.tce@gmail.com>
2022-04-24 17:30:42 +0530

    second

--- src/main/java/com/capg/Controller/EmployeeController.java
+++ src/main/java/com/capg/Controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.capg.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employee")
 public class EmployeeController {
 	
-	@GetMapping
+	@GetMapping("/getEmployee")
 	public ResponseEntity<String> getEmployee()
 	{
 		return new ResponseEntity<String>("Employee details are here..",HttpStatus.OK);
+		
+	}
+	@GetMapping("welcome/{name}")
+	
+	public ResponseEntity<String> getEmployee(@PathVariable("name")String name)
+	{
+		return new ResponseEntity<String>("welcome "+name,HttpStatus.OK);
+		
 	}
 
 }
